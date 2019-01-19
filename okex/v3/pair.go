cc7f960694d25d089442f5d902b08ccde4085277
wan lu cheng <wanlucheng@wanludeMacBook-Pro-5.local>
2019-01-19 17:39:37 +0800

    Add method: Instruments

---
+++ okex/v3/pair.go
@@ -0,0 +1,29 @@
+package okex
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/pkg/errors"
+)
+
+// Instruments Get market data. This endpoint provides the snapshots of market data and can be used without verifications.
+// List trading pairs and get the trading limit, price, and more information of different trading pairs.
+func (r *rest) Instruments() ([]*Instrument, error) {
+	method := http.MethodGet
+	path := "/api/spot/v3/instruments"
+	content, err := r.Request(method, path, nil, nil, false)
+	if err != nil {
+		if _, ok := err.(ErrResponse); ok {
+			return nil, err
+		}
+		return nil, errors.Wrap(err, "instruments request")
+	}
+
+	var ss []*Instrument
+	if err := json.Unmarshal(content, &ss); err != nil {
+		return nil, errors.Wrap(err, "Instruments response body")
+	}
+
+	return ss, nil
+}
